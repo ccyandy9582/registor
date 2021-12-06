a982deea428c08539867ba12884880bfd844b492
ccyandy9582 <ccyandy9582@gmail.com>
2021-12-06 23:51:24 +0800

    try to appliy axios.create()

--- src/components/applierList/ApplierList.js
+++ src/components/applierList/ApplierList.js
@@ -1,59 +1,62 @@
-import { useEffect, useState } from 'react';
-import { deleteApplication, getApplications } from '../../service/data';
+import {useEffect, useState} from 'react';
+import {deleteApplication, getApplications} from '../../service/data';
 
-export default function ApplierList({ selectedDate, selectedTime, applierList, setApplierList }) {
-	const [ isLoading, setIsLoading ] = useState(true);
+export default function ApplierList({selectedDate, selectedTime, applierList, setApplierList}) {
+  const [isLoading, setIsLoading] = useState(true);
 
-	const deleteBtnOnclick = (event) => {
-		deleteApplication(event.target.id);
-	};
+  const deleteBtnOnclick = (event) => {
+    if (window.confirm(`你確定要退出嗎？`)) {
+      deleteApplication(event.target.id).then(r => alert(r));
+    }
+  };
 
-	useEffect(
-		() => {
-			async function getApplicationsData() {
-				let day = '0' + selectedDate.getDate();
-				const correctDate = `${selectedDate.getFullYear()}-${selectedDate.getMonth() + 1}-${day.slice(-2)}`;
-				setIsLoading(true);
-				setApplierList([]);
-				const applications = await getApplications();
-				let tempList = [];
-				setIsLoading(false);
-				applications.data.forEach((application) => {
-					if (selectedTime) {
-						if (
-							correctDate == application.date.split('T')[0] &&
-							selectedTime.slice(0, 11) == application.time
-						)
-							tempList.push(application.name + ',' + application._id);
-					}
-				});
-				setApplierList(tempList);
-			}
-			getApplicationsData();
-		},
-		[ selectedTime ]
-	);
+  useEffect(
+    () => {
+      async function getApplicationsData() {
+        let day = '0' + selectedDate.getDate();
+        const correctDate = `${selectedDate.getFullYear()}-${selectedDate.getMonth() + 1}-${day.slice(-2)}`;
+        setIsLoading(true);
+        setApplierList([]);
+        const applications = await getApplications();
+        let tempList = [];
+        setIsLoading(false);
+        applications.data.forEach((application) => {
+          if (selectedTime) {
+            if (
+              correctDate === application.date.split('T')[0] &&
+              selectedTime.slice(0, 11) === application.time
+            )
+              tempList.push(application.name + ',' + application._id);
+          }
+        });
+        setApplierList(tempList);
+      }
 
-	if (!isLoading) {
-		if (selectedDate && selectedTime) {
-			if (applierList.length > 0) {
-				return applierList.map((details, index) => {
-					let name = details.split(',')[0];
-					let id = details.split(',')[1];
-					return (
-						<div align={'left'} key={name + id + index}>
-							<li>
-								{name}&emsp;
-								<button id={id} onClick={deleteBtnOnclick}>
-									刪除
-								</button>
-							</li>
-						</div>
-					);
-				});
-			} else return '尚未有人報名';
-		} else {
-			return null;
-		}
-	} else return 'loading...';
+      getApplicationsData();
+    },
+    [selectedTime]
+  );
+
+  if (!isLoading) {
+    if (selectedDate && selectedTime) {
+      if (applierList.length > 0) {
+        return applierList.map((details, index) => {
+          let name = details.split(',')[0];
+          let id = details.split(',')[1];
+          return (
+            <div align={'left'} key={name + id + index}>
+              <li>
+                {name}&emsp;
+                <button id={id} onClick={deleteBtnOnclick}>
+                  刪除
+                </button>
+              </li>
+            </div>
+          );
+        });
+      } else return '尚未有人報名';
+    } else {
+      return null;
+    }
+  } else return 'loading...';
 }
--- src/service/data.js
+++ src/service/data.js
@@ -1,84 +1,96 @@
 import axios from 'axios';
 
-const baseURL = 'http://3.140.181.237:8083/';
+const CancelToken = axios.CancelToken;
+const source = CancelToken.source();
+
+const applicationApi = axios.create({
+  baseURL: 'http://3.140.181.237:8083',
+  headers: {'Content-Type': "application/json"}
+})
 
 export const getApplications = () => {
-	return new Promise((resolve, reject) => {
-		axios({
-			method : 'get',
-			url    : baseURL + 'getAllAppliers'
-		})
-			.then((res) => {
-				resolve(res);
-			})
-			.catch((err) => {
-				reject(err);
-			});
-	});
+  return applicationApi.get('/getAllAppliers')
+  // return new Promise((resolve, reject) => {
+  //   axios({
+  //     method: 'get',
+  //     url: baseURL + 'getAllAppliers'
+  //   })
+  //     .then((res) => {
+  //       resolve(res);
+  //     })
+  //     .catch((err) => {
+  //       reject(err);
+  //     });
+  // });
 };
 
 export const getAvalibleTime = (weekday = '') => {
-	return new Promise((resolve, reject) => {
-		axios({
-			url    : baseURL + 'getAvailableTime/' + weekday.toUpperCase(),
-			method : 'get'
-		})
-			.then((res) => {
-				resolve(res);
-			})
-			.catch((err) => {
-				reject(err);
-			});
-	});
+  return applicationApi.get(`/getAvailableTime/${weekday.toUpperCase()}`)
+  // return new Promise((resolve, reject) => {
+  //   axios({
+  //     url: baseURL + 'getAvailableTime/' + weekday.toUpperCase(),
+  //     method: 'get'
+  //   })
+  //     .then((res) => {
+  //       resolve(res);
+  //     })
+  //     .catch((err) => {
+  //       reject(err);
+  //     });
+  // });
 };
 
-export const getApplicationsByDate = (date) => {
-	return new Promise((resolve, reject) => {
-		axios({
-			url    : baseURL + 'getApplierByDate/' + date,
-			method : 'get'
-		})
-			.then((res) => {
-				resolve(res);
-			})
-			.catch((err) => {
-				reject(err);
-			});
-	});
-};
+// export const getApplicationsByDate = (date) => {
+// return new Promise((resolve, reject) => {
+//   axios({
+//     url: baseURL + 'getApplierByDate/' + date,
+//     method: 'get'
+//   })
+//     .then((res) => {
+//       resolve(res);
+//     })
+//     .catch((err) => {
+//       reject(err);
+//     });
+// });
+// };
 
 export const deleteApplication = (_id) => {
-	return new Promise((resolve, reject) => {
-		axios({
-			url    : baseURL + 'deleteApplication/' + _id,
-			method : 'delete'
-		})
-			.then((res) => {
-				resolve(res);
-			})
-			.catch((err) => {
-				reject(err);
-			});
-	});
+  return applicationApi.delete(`/deleteApplication/${_id}`, {
+    cancelToken: source.token
+  })
+  // return new Promise((resolve, reject) => {
+  //   axios({
+  //     url: baseURL + 'deleteApplication/' + _id,
+  //     method: 'delete'
+  //   })
+  //     .then((res) => {
+  //       resolve(res);
+  //     })
+  //     .catch((err) => {
+  //       reject(err);
+  //     });
+  // });
 };
 
 export const newApplication = (date, name, time) => {
-	alert(`/newApplication\n${date}\n${name}\n${time}`);
-	return new Promise((resolve, reject) => {
-		axios({
-			url    : baseURL + 'newApplication/',
-			method : 'POST',
-			data   : {
-				date,
-				name,
-				time
-			}
-		})
-			.then((res) => {
-				resolve(res);
-			})
-			.catch((err) => {
-				reject(err);
-			});
-	});
+  return applicationApi.post('/newApplication', {date, name, time})
+  // alert(`/newApplication\n${date}\n${name}\n${time}`);
+  // return new Promise((resolve, reject) => {
+  //   axios({
+  //     url: baseURL + 'newApplication/',
+  //     method: 'POST',
+  //     data: {
+  //       date,
+  //       name,
+  //       time
+  //     }
+  //   })
+  //     .then((res) => {
+  //       resolve(res);
+  //     })
+  //     .catch((err) => {
+  //       reject(err);
+  //     });
+  // });
 };
