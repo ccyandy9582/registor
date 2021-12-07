e19d0b8f886e3893523a28e1a8c80105b77e2dd1
ccyandy9582 <ccyandy9582@gmail.com>
2021-12-07 17:32:19 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 // import { Auth } from './index';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
@@ -7,12 +7,24 @@ import TimeSlot from './components/timeSlot/TimeSlot';
 import { getAvalibleTime } from './service/data';
 import './App.css';
 
+export const ApplicationContext = createContext({
+  test: "testing"
+})
+
 export default function App() {
 	const [ selectedDate, setSelectedDate ] = useState(new Date());
 	const [ timeSlot, setTimeSlot ] = useState([]);
 	const [ selectedTime, setSelectedTime ] = useState();
 	const [ applierList, setApplierList ] = useState([]);
 	const [ isLoading, setIsLoading ] = useState(false);
+
+  const applicationContext = {
+    selectedDate, setSelectedDate,
+    selectedTime, setSelectedTime,
+    timeSlot, setTimeSlot,
+    applierList, setApplierList,
+    isLoading, setIsLoading
+  }
 
 	const timeSlotOnChange = (event) => {
 		console.log(event.target.value);
@@ -52,55 +64,76 @@ export default function App() {
 		[ selectedDate ]
 	);
 
-	if (isLoading) {
-		return (
-			<form>
-				<Calendar
-					className="myCal"
-					minDetail="year"
-					calendarType={'US'}
-					value={selectedDate}
-					onChange={(value) => {
-						setApplierList([]);
-						setSelectedDate(value);
-					}}
-				/>
-				<ApplyEvent
-					selectedTime={selectedTime}
-					setSelectedTime={setSelectedTime}
-					style={{ marginTop: '10px' }}
-				/>
-				<br />
-				loading ...
-			</form>
-		);
-	} else {
-		return (
-			<form align={'center'}>
-				<h1>金剛棒</h1>
-				<Calendar
-					className="myCal"
-					minDetail="year"
-					calendarType={'US'}
-					value={selectedDate}
-					onChange={(value) => {
-						setApplierList([]);
-						setSelectedTime();
-						setSelectedDate(value);
-					}}
-				/>
-				<ApplyEvent selectedDate={selectedDate} selectedTime={selectedTime} />
-				<br />
-				<TimeSlot
-					selectedDate={selectedDate}
-					timeSlot={timeSlot}
-					setTimeSlot={setTimeSlot}
-					selectedTime={selectedTime}
-					setSelectedTime={setSelectedTime}
-					applierList={applierList}
-					setApplierList={setApplierList}
-				/>
-			</form>
-		);
-	}
+
+  <ApplicationContext.Provider value={applicationContext}>
+    <h1>金剛棒</h1>
+    <form>
+      <Calendar/>
+      {isLoading?"loading ...":<>
+        <ApplyEvent selectedDate={selectedDate} selectedTime={selectedTime} /><br />
+        <TimeSlot
+          selectedDate={selectedDate}
+          timeSlot={timeSlot}
+          setTimeSlot={setTimeSlot}
+          selectedTime={selectedTime}
+          setSelectedTime={setSelectedTime}
+          applierList={applierList}
+          setApplierList={setApplierList} />
+        </>}
+    </form>
+  </ApplicationContext.Provider>
+
+
+	// if (isLoading) {
+	// 	return (
+	// 		<form>
+	// 			<h1>金剛棒</h1>
+	// 			<Calendar
+	// 				className="myCal"
+	// 				minDetail="year"
+	// 				calendarType={'US'}
+	// 				value={selectedDate}
+	// 				onChange={(value) => {
+	// 					setApplierList([]);
+	// 					setSelectedDate(value);
+	// 				}}
+	// 			/>
+	// 			<ApplyEvent
+	// 				selectedTime={selectedTime}
+	// 				setSelectedTime={setSelectedTime}
+	// 				style={{ marginTop: '10px' }}
+	// 			/>
+	// 			<br />
+	// 			loading ...
+	// 		</form>
+	// 	);
+	// } else {
+	// 	return (
+	// 		<form align={'center'}>
+	// 			<h1>金剛棒</h1>
+	// 			<Calendar
+	// 				className="myCal"
+	// 				minDetail="year"
+	// 				calendarType={'US'}
+	// 				value={selectedDate}
+	// 				onChange={(value) => {
+	// 					setApplierList([]);
+	// 					setSelectedTime();
+	// 					setSelectedDate(value);
+	// 				}}
+	// 			/>
+	// 			<ApplyEvent selectedDate={selectedDate} selectedTime={selectedTime} />
+	// 			<br />
+	// 			<TimeSlot
+	// 				selectedDate={selectedDate}
+	// 				timeSlot={timeSlot}
+	// 				setTimeSlot={setTimeSlot}
+	// 				selectedTime={selectedTime}
+	// 				setSelectedTime={setSelectedTime}
+	// 				applierList={applierList}
+	// 				setApplierList={setApplierList}
+	// 			/>
+	// 		</form>
+	// 	);
+	// }
 }
